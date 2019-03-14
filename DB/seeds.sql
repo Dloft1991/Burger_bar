09ec265aacaa3d184ac6899fe820841a3584c9a4
Dloft <derek.lofton1@gmail.com>
2019-03-14 08:38:45 -0400

    new

---
+++ DB/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('Plain burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Canadian bacon burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Florida burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rum burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Swiss burger');
